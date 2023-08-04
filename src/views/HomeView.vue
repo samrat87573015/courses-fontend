ff4d520fe58fe917d0c0bff6be5c8b60aed8b6fa
samrat87573015 <www.samrat1998@gmail.com>
2023-08-04 12:17:34 +0600

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <section>
+      <div class="banner_area overlay h-[700px] flex items-center !bg-cover !bg-center !bg-no-repeat">
+        <div class="container">
+          <div class="gird grid-cols-1 items-center">
+            <div class="space-y-5 banner_content relative z-10">
+              <h6 class="text-white text-xl font-medium">For Your Better Life</h6>
+              <h1 class="text-7xl text-white font-bold">Build Better Life Today<br>
+                with <span class="text-[#eba504]">Jessica Hische</span></h1>
+              <div class="btn_area">
+                <RouterLink to="/courses" class="btn">All Courses</RouterLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
